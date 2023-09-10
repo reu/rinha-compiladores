3937382c69e26e8e1b9bfb3fb6de3c48dd49da4f
Rodrigo Navarro <rnavarro1@gmail.com>
2023-09-10 10:40:41 -0300

    Hello world done!

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![allow(unused)]
+
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct File {
+    name: String,
+    expression: Term,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Int {
+    value: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Str {
+    value: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Print {
+    value: Box<Term>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "kind")]
+pub enum Term {
+    Int(Int),
+    Str(Str),
+    Print(Print),
+}
+
+#[derive(Debug)]
+pub enum Val {
+    Void,
+    Int(i32),
+    Bool(bool),
+    Str(String),
+}
+
+fn eval(term: Term) -> Val {
+    match term {
+        Term::Int(number) => Val::Int(number.value),
+        Term::Str(str) => Val::Str(str.value),
+        Term::Print(print) => {
+            let val = eval(*print.value);
+            match val {
+                Val::Int(n) => print!("{n}"),
+                Val::Bool(b) => print!("{b}"),
+                Val::Str(s) => print!("{s}"),
+                _ => panic!("valor não suportado"),
+            };
+            Val::Void
+        },
+    }
+}
+
+fn main() {
+    let program = fs::read_to_string("./examples/hello.json").unwrap();
+    let program = serde_json::from_str::<File>(&program).unwrap();
+
+    let term = program.expression;
+    eval(term);
+}
